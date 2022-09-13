7b03ed1c1a4c53de39005b0da2aea9961bcc1fa7
EricRV33 <ericromero232434@gmail.com>
2022-09-13 14:45:51 -0500

    Tarea 1 finish

---
+++ Tarea1_Suma_Eric/Tarea1_Suma_Eric/Form1.cs
@@ -0,0 +1,50 @@
+namespace Tarea1_Suma_Eric
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1,resultado, n3;
+
+            n1=double.Parse(textBox1.Text);
+            n3=double.Parse(textBox3.Text);
+            resultado = n1 + n3; 
+            textBox2.Text = resultado.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
